313ef119915304881db8078c2e5be40bad0c471f
Phil <nephxio@users.noreply.github.com>
2017-04-28 00:20:13 -0400

    Initial Commit of Files
    Uploading Project Files

---
+++ Source/projectllama/projectllamaGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "projectllama.h"
+#include "projectllamaGameModeBase.h"
+
+
+
+
---
+++ Source/projectllama/projectllamaGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "projectllamaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTLLAMA_API AprojectllamaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
